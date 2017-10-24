5b6fb03b1df9b91d8d34ae1acfa94b5df404fd0b
guduf <gudufour@gmail.com>
2017-10-24 15:15:28 +0200

    fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,11 @@
 export { bootstrapGrapp } from './root';
 export { Data, DataArray } from './data_fields';
 export { Db } from './db';
-export { Inject, Payload, Typer } from './di';
+export { Inject, Payload, Provider, Typer } from './di';
 export { Collection, DocMutation, DocQuery, DocSubscription } from './doc_di';
 export { decorateDoc as Doc, docDocIdDecorator as DocId } from './doc';
 export { decorateGrapp as Grapp } from './grapp';
 export { decorateMutation as Mutation, decorateQuery as Query, decorateSubscription as Subscription } from './operation';
-export { PubSub } from './pubsub';
 export { decorateType as Type } from './type';
 export { Relation } from './relation_fields';
 export { onConnect, onDisconnect, onOperation, onOperationComplete } from './options';
