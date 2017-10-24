1178e60effe46cd5417476fea98997d768eeaf40
guduf <gudufour@gmail.com>
2017-10-24 17:11:19 +0200

    downgrade ws

--- lib/options.d.ts
+++ lib/options.d.ts
@@ -1,10 +1,9 @@
-/// <reference types="ws" />
 import * as WebSocket from 'ws';
 import { OperationMessage } from 'subscriptions-transport-ws/dist/server';
 export declare const GRAPP_OPERATIONS: symbol;
 export declare type GrappOperations = Map<string, Function>;
 export declare function onConnect(connectionParams: Object, ws: WebSocket): {
-    ws: WebSocket;
+    ws: any;
 };
 export declare function onOperation({id, payload}: OperationMessage, params: {
     [key: string]: any;
