79144563d43dba62e75fa413547c8f5906c57b4f
guduf <gudufour@gmail.com>
2017-10-26 09:20:46 +0200

    compile lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,11 @@
-export { bootstrapGrapp } from './root';
 export { Data, DataArray } from './data_fields';
 export { Db } from './db';
 export { Inject, Payload, Provider, Typer } from './di';
-export { Collection, DocMutation, DocQuery, DocSubscription } from './doc_di';
 export { decorateDoc as Doc, docDocIdDecorator as DocId } from './doc';
+export { Collection, DocMutation, DocQuery, DocSubscription } from './doc_di';
+export { DocEvent, DocEvents } from './doc_event';
 export { decorateGrapp as Grapp } from './grapp';
 export { decorateMutation as Mutation, decorateQuery as Query, decorateSubscription as Subscription } from './operation';
 export { decorateType as Type } from './type';
 export { Relation } from './relation_fields';
-export { onConnect, onDisconnect, onOperation, onOperationComplete } from './options';
+export { bootstrapGrapp } from './root';
