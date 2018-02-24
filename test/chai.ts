c34306ae044cd218260098128b08b09a9fac0abd
guduf <gudufour@gmail.com>
2018-02-24 09:45:57 +0100

    remove mocha for jest

--- test/chai.ts
+++
@@ -1,5 +0,0 @@
-import 'mocha';
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
-chai.use(chaiAsPromised);
-chai.should();
--- test/field.spec.ts
+++
@@ -1,9 +0,0 @@
-import './chai';
-
-import { FieldMeta, FIELDS_META } from '../src/field';
-import { mapMeta } from '../src/meta';
-import { decorateType } from '../src/type';
-
-describe('field.ts', function() {
-
-});
